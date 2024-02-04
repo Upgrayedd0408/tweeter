bf8c3483cddd2449fa18bbcb60a730b499ea6530
Upgrayedd0408 <michaellowry89@gmail.com>
2024-02-04 17:11:43 +0000

    add renderTweets and createTweetElement functions and display them on main page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,97 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1706859768749
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1706946168749
+    }
+  ];
 
+  function createTweetElement(tweetData) {
+    const formattedDate = moment(tweetData.created_at).fromNow();
+
+    const elementHTML = `
+      <article class="tweet">
+        <header>
+          <div>
+            <img src=${tweetData.user.avatars}>
+            <h3>${tweetData.user.name}</h3>
+          </div>
+          <h3>${tweetData.user.handle}</h3>
+        </header>
+
+          <p>${tweetData.content.text}</p>
+
+        <footer>
+          <h6>${formattedDate}</h6>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+
+      </article>
+  `;
+
+    return $(elementHTML);
+  }
+
+/*   const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1706859768749
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1706946168749
+    }
+  ]; */
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(function(tweet) {
+      // Here, call createTweetElement for each tweet
+      const $tweetElement = createTweetElement(tweet);
+      // Then, append it to the tweets container
+      $('.tweets-container').append($tweetElement);
+    });
+  }
+
+  renderTweets(data);
+
+
+});
